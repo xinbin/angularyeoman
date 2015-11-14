4caccb8137fe73f6f4ea3f513dd8dd6867c63888
adrianna <adrianna.flores@rackspace.com>
2015-11-14 13:40:30 -0600

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularyeomanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularyeomanApp
+ */
+angular.module('angularyeomanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularyeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularyeomanApp
+ */
+angular.module('angularyeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
